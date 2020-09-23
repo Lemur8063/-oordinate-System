dc2b029a73bbd50a5a07af46fddacb9ec55375e0
Lemur8063 <oleg_valahanovich@mail.ru>
2020-09-23 15:50:16 +0300

    fix

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-let elements = [{ name: 'Circle', x: '2', y: '4' },
-                     { name: 'Square', x: '4', y: '2' },
-                        {name: 'Triangle', x: '5', y: '7'}];
-
-
-
+let elements = [{ name: 'circle', x: '1', y: '1' },
+                     { name: 'square', x: '4', y: '2' },
+                        {name: 'triangle', x: '5', y: '7'}];
 function getObjectsByXY (x,y) {
     for (let element of elements) {
-        if (element.x == x && element.y == y) {
+        if (element.x === x && element.y === y) {
             return element.name;
         }
-        else {
-            console.log ("Элемент не найден!");
-        }
     }    
-    //let object = document.getElementsByClassName("circle");
-    //let style = getComputedStyle(object[0]);
-    //console.log(style.left);
 }
-function setObjectsByXY (x,y) {
+function setObjectsByXY (object,x,y) {
+        for (let element of elements) {
+            if (element.name === object) {
+                element.x = x;
+                element.y = y;
+            }
+        }    
 }
-function updateObjectXY (object,x,y) {
+function updateObjectXY (object) {
+    for (let element of elements) {
+        if (element.name === object) {
+        x = document.getElementById(`${object}`).style.marginBottom = element.x * 33 + "px";
+          y =  document.getElementById(`${object}`).style.left = element.y * 33 + "px";;
+            console.log(left,bottom,object);
+        }
+    }  
+}
+button = document.querySelector('.button');
+button.onclick = function() {
+    let currentX = document.getElementsByClassName('current-x');
+    let currentY = document.getElementsByClassName('current-y');
+    let newX = document.getElementsByClassName('new-x');
+    let newY = document.getElementsByClassName('new-y');
+    let object = getObjectsByXY(currentX[0].value,currentY[0].value);
+    setObjectsByXY(object,newX[0].value,newY[0].value);
+    updateObjectXY (object);
+};
 
-}
