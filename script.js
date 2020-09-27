2a467e3e096b51dcfc00c481bd81e1f0fa1b2d52
Lemur8063 <oleg_valahanovich@mail.ru>
2020-09-27 15:18:43 +0300

    fix

--- script.js
+++ script.js
@@ -20,19 +20,41 @@ function updateObjectXY (object) {
     for (let element of elements) {
         if (element.name === object) {
         x = document.getElementById(`${object}`).style.marginBottom = element.x * 33 + "px";
-          y =  document.getElementById(`${object}`).style.left = element.y * 33 + "px";;
-            console.log(left,bottom,object);
+          y =  document.getElementById(`${object}`).style.marginLeft = element.y * 33 + "px";
         }
-    }  
+    }
+} 
+function error (text,visibility) {
+    let error = document.querySelector(".error");
+    if (visibility) {
+    error.style.opacity = "1";
+    error.textContent = text;
+    error.innerHTML +="<br/>";
+    } else {
+        error.style.opacity = "0";
+        error.textContent = "";
+        error.innerHTML ="";
+    }
 }
+
 button = document.querySelector('.button');
 button.onclick = function() {
     let currentX = document.getElementsByClassName('current-x');
     let currentY = document.getElementsByClassName('current-y');
     let newX = document.getElementsByClassName('new-x');
     let newY = document.getElementsByClassName('new-y');
+    if (currentX[0].value <= 15 && currentY[0].value <= 15 &&
+        currentX[0].value >= 1 && currentY[0].value >= 1) {
+        if (newX[0].value <= 15 && newY[0].value <= 15 &&
+            newX[0].value >= 1 && newY[0].value >= 1) {
     let object = getObjectsByXY(currentX[0].value,currentY[0].value);
     setObjectsByXY(object,newX[0].value,newY[0].value);
     updateObjectXY (object);
+    error("", false);
+    } else {
+        text = "* Элемент выходит за границы сетки. Введите другие координаты и попробуйте снова."
+        error(text, true);
+    }
+}
 };
 
